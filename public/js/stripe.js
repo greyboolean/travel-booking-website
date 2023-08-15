5ac38825529e2820c9357ccaa1f17367f005788f
Grey Boolean <greyboolean@gmail.com>
2023-08-15 19:43:09 +0530

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51NeEXZCH1BRLFnj4O1fUGc3xBN41v6nXIVguNCOU8hUco3bpxv3lTEOxFCUK53YTcHhPBzUCfU03zTQWPPipTAMy00UB8A3gVx'
+);
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
